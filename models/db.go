3563aac5962d8a9065d4debbf821a86328ea1c12
Hao yun <hy352144278@gmail.com>
2019-08-05 09:38:07 +0800

    Fix: fix db instance get method

--- models/db.go
+++ models/db.go
@@ -3,11 +3,28 @@ package models
 import (
 	"fmt"
 
+	"github.com/astaxie/beego"
+	"github.com/golang/glog"
 	"github.com/jinzhu/gorm"
+
+	// import dialects
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var conn *gorm.DB
+
+// InitDB init DB
+func InitDB() (db *gorm.DB, e error) {
+	dbHost := beego.AppConfig.String("dbHost")
+	dbUser := beego.AppConfig.String("dbUser")
+	dbName := beego.AppConfig.String("dbName")
+	dbPass := beego.AppConfig.String("dbPass")
+	conn, err := Connect(dbHost, dbName, dbUser, dbPass)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
 
 // Connect connect to the postgresql
 func Connect(host string, database string, user string, pass string) (db *gorm.DB, err error) {
@@ -20,5 +37,12 @@ func Connect(host string, database string, user string, pass string) (db *gorm.D
 
 // GetDb return the db instance
 func GetDb() *gorm.DB {
+	var err error
+	if conn == nil || conn.DB().Ping() != nil {
+		conn, err = InitDB()
+		if err != nil {
+			glog.Error("db init error")
+		}
+	}
 	return conn
 }
